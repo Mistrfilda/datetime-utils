9d1ae888e28658aeb689c4f60a2b65cae62b0022
filip <filda.kuchar@seznam.cz>
2020-12-05 19:28:50 +0100

    init

---
+++ src/DatetimeException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mistrfilda\Datetime;
+
+use Exception;
+
+class DatetimeException extends Exception
+{
+}
---
+++ src/DatetimeFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mistrfilda\Datetime;
+
+use DateTimeImmutable;
+
+class DatetimeFactory
+{
+	public const DEFAULT_DATETIME_FORMAT = 'Y-m-d H:i:s';
+
+	public const DEPARTURE_TABLE_DATETIME_FORMAT = 'd. m. Y H:i:s';
+
+	public const DEFAULT_DATE_FORMAT = 'Y-m-d';
+
+	public const DEFAULT_NULL_DATETIME_PLACEHOLDER = '---';
+
+	public const DEFAULT_MYSQL_DATETIME_FORMAT = 'Y-m-d H:i:s';
+
+	public function createNow(): DateTimeImmutable
+	{
+		return new DateTimeImmutable();
+	}
+
+	public function createToday(): DateTimeImmutable
+	{
+		return (new DateTimeImmutable())->setTime(0, 0, 0);
+	}
+
+	public function createFromTimestamp(int $timestamp): DateTimeImmutable
+	{
+		return new DateTimeImmutable('@' . $timestamp);
+	}
+
+	public function createDatetimeFromMysqlFormat(
+		string $datetime,
+		string $mysqlDatetimeFormat = self::DEFAULT_MYSQL_DATETIME_FORMAT
+	): DateTimeImmutable {
+		$parsedDatetime = DateTimeImmutable::createFromFormat($mysqlDatetimeFormat, $datetime);
+		if ($parsedDatetime === false) {
+			throw new DatetimeException('Can\t create datetome from specified value and format');
+		}
+
+		return $parsedDatetime;
+	}
+}
